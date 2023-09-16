0337bb4380b2ecc857984fdb2bbfc7a9a1837b7f
Hchess1919 <hccalisgan06@gmail.com>
2023-09-16 14:10:32 +0300

    123

---
+++ asd.py
@@ -0,0 +1,23 @@
+import random
+
+
+
+tutulan=random.randint(1,100)
+print("1-100 arası bir sayı tuttum 5 hakkın var")
+
+
+
+for hak in range (5):
+    tahmin=int(input("tahminin ne"))
+    if tahmin== tutulan:
+        print("bildin")
+        break
+    elif tahmin<tutulan: 
+        print("yukarıda")
+    elif tahmin>tutulan: print ("aşağıda")
+
+
+print(':( uzuldum ama 5 hakinda bitti :(')
+print('💔💔💔💔💔')
+print('cevap', tutulan)
+exit()
